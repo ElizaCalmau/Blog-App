9fab529b47984b626f225bc66771ca5e3d703e2f
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-18 19:28:43 +0200

    created constants and added header items

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,7 @@
+export const HEADER_ITEMS = [
+    { name: 'Home', path: '/' },
+    { name: 'Sign Up', path: '/sign-up' },
+    { name: 'Sign In', path: '/sign-in' },
+    { name: 'About', path: '/about' },
+    { name: 'Contacts', path: '/contacts' },
+]
