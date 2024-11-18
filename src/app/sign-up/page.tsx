8a1eaa19532c8933fbe23b1b1226cf319d6d8558
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-18 16:58:47 +0200

    used Auth form componend On Sing Up page

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,61 +1,10 @@
 'use client'
 
-import { useHandleSignUp } from '../utils/handleSignUp'
+import { AuthForm } from '../components/AuthForm'
+import { useHandleSignUp } from '../utils/useHandleSignUp'
 
 const SignUp = () => {
-    const { handleSignUp, email, setEmail, password, setPassword } =
-        useHandleSignUp()
-
-    return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <form
-                onSubmit={handleSignUp}
-                className="bg-white p-8 rounded shadow-md w-80"
-            >
-                <h2 className="text-2xl font-semibold mb-6 text-center">
-                    Sign Up
-                </h2>
-
-                <div className="mb-4">
-                    <label className="block text-gray-700 mb-2" htmlFor="email">
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:ring-blue-200"
-                        required
-                    />
-                </div>
-
-                <div className="mb-6">
-                    <label
-                        className="block text-gray-700 mb-2"
-                        htmlFor="password"
-                    >
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:ring-blue-200"
-                        required
-                    />
-                </div>
-
-                <button
-                    type="submit"
-                    className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition-colors"
-                >
-                    Sign Up
-                </button>
-            </form>
-        </div>
-    )
+    return <AuthForm title="Sing Up" authFunction={useHandleSignUp} />
 }
 
 export default SignUp
