2130c8536076ddfd6421e957c69f61f2e0250242
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-18 16:12:45 +0200

    created useHadleSignUp

---
+++ src/app/utils/handleSignUp.ts
@@ -0,0 +1,26 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { useState } from 'react'
+import { auth } from '../firebase/firebase'
+export const useHandleSignUp = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSignUp = (event: React.FormEvent) => {
+        event.preventDefault()
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user
+                console.log('user', user)
+                setEmail('')
+                setPassword('')
+            })
+            .catch((error) => {
+                const errorCode = error.code
+                const errorMessage = error.message
+                // ..
+                console.error('error', errorCode, errorMessage)
+            })
+    }
+
+    return { email, setEmail, password, setPassword, handleSignUp }
+}
