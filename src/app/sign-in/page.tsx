19ea50585ea0d57201148f7ff67b6b93faa3c1f4
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-18 15:28:24 +0200

    added accordion for sign in

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { auth } from '../firebase/firebase'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { useRouter } from 'next/navigation'
+import { Accordion } from '../components/Accordion'
 
 const SignIn = () => {
     const router = useRouter()
@@ -29,52 +30,57 @@ const SignIn = () => {
 
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <form
-                onSubmit={handleSignIn}
-                className="bg-white p-8 rounded shadow-md w-80"
-            >
-                <h2 className="text-2xl font-semibold mb-6 text-center">
-                    Sign In
-                </h2>
-
-                <div className="mb-4">
-                    <label className="block text-gray-700 mb-2" htmlFor="email">
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:ring-blue-200"
-                        required
-                    />
-                </div>
-
-                <div className="mb-6">
-                    <label
-                        className="block text-gray-700 mb-2"
-                        htmlFor="password"
-                    >
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:ring-blue-200"
-                        required
-                    />
-                </div>
-
-                <button
-                    type="submit"
-                    className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition-colors"
+            <Accordion title="Sign Up With Email">
+                <form
+                    onSubmit={handleSignIn}
+                    className="bg-white p-8 rounded shadow-md w-80"
                 >
-                    Sign In
-                </button>
-            </form>
+                    <h2 className="text-2xl font-semibold mb-6 text-center">
+                        Sign In
+                    </h2>
+
+                    <div className="mb-4">
+                        <label
+                            className="block text-gray-700 mb-2"
+                            htmlFor="email"
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:ring-blue-200"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-6">
+                        <label
+                            className="block text-gray-700 mb-2"
+                            htmlFor="password"
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:ring-blue-200"
+                            required
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition-colors"
+                    >
+                        Sign In
+                    </button>
+                </form>
+            </Accordion>
         </div>
     )
 }
