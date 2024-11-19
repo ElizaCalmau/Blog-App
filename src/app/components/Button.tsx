b8de654e567f5df3b337b6486f02d2bde8084926
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-19 17:53:38 +0200

    reduced props for Button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,27 +1,19 @@
-import { ROUTES } from '../constants/constants'
-
 interface ButtonProp {
     text: string
-    color: string
-    size: string
-    padding: string
-    width: string
-    position?: string
-    onClick: React.MouseEventHandler<HTMLDivElement>
+    styles: string
+    onClick?: React.MouseEventHandler<HTMLDivElement>
+    type?: string
 }
 
 export const Button: React.FC<ButtonProp> = ({
     text,
-    color,
-    size,
-    padding,
-    width,
-    position,
+    styles,
     onClick,
+    type,
 }) => {
     return (
         <div
-            className={`${color} ${size} ${width} ${padding} ${position} rounded-3xl text-center cursor-pointer`}
+            className={`${styles} text-center cursor-pointer`}
             onClick={onClick}
         >
             {text}
