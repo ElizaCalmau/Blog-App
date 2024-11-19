3439a3dc1f97ae06ff8c96162893714c049cd5f5
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-19 14:59:13 +0200

    added custom colors and customized grid rows

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
+                accent: '#D76060',
+                primary: '#8C8C8C',
+                secondary: '#C49191',
+            },
+            gridTemplateRows: {
+                '4': 'repeat(4, minmax(0, 0.4fr))',
       },
     },
   },
