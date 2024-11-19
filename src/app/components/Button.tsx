7d9bc503a04a22008b6b0362342dcdfbb61b51b6
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-19 15:01:11 +0200

    created button component

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,25 @@
+interface Prop {
+    text: string
+    color: string
+    size: string
+    padding: string
+    width: string
+    position?: string
+}
+
+export const Button: React.FC<Prop> = ({
+    text,
+    color,
+    size,
+    padding,
+    width,
+    position,
+}) => {
+    return (
+        <div
+            className={`${color} ${size} ${width} ${padding} ${position} rounded-3xl w-fit text-center cursor-pointer`}
+        >
+            {text}
+        </div>
+    )
+}
