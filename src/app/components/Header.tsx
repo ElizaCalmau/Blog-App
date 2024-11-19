1670579d4d22831d70f41a8e7c8771fb8a965dff
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-19 15:00:38 +0200

    updated styles for header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,16 +8,14 @@ export const Header: React.FC = () => {
     const path: string = usePathname()
 
     return (
-        <div className="fixed top-0 p-5 flex space-x-4">
+        <div className="flex space-x-6">
             {HEADER_ITEMS.map((item) => (
                 <Link
                     key={item.name}
                     href={item.path}
                     className={`${
-                        path === item.path
-                            ? 'text-red-400 font-bold'
-                            : 'text-black'
-                    }`}
+                        path === item.path ? 'text-accent' : 'text-black'
+                    } text-xl font-bold`}
                 >
                     {item.name}
                 </Link>
