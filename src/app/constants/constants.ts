0f9a47885b1a3bdbec649e86c8d5d986c471807b
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2024-11-19 15:00:18 +0200

    added values to constants.ts

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,7 +1,13 @@
 export const HEADER_ITEMS = [
     { name: 'Home', path: '/' },
-    { name: 'Sign Up', path: '/sign-up' },
     { name: 'Sign In', path: '/sign-in' },
     { name: 'About', path: '/about' },
     { name: 'Contacts', path: '/contacts' },
 ]
+
+export const HOME_DESCRIPRION =
+    'A blog dedicated to exploring the art of connecting through social media—tips, trends, and insights'
+
+export const SIGN_UP = 'Sign Up'
+export const SIGN_IN = 'Sign In'
+export const SIGN_OUT = 'Sign OUT'
